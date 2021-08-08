b6adbcbd5488c94a4f2ee966b13e0892817271a8
lvzhen <15549806382@qq.com>
2021-08-08 20:02:38 +0800

    feat:api

--- keras-flask-deploy-webapp/app.py
+++ keras-flask-deploy-webapp/app.py
@@ -4,6 +4,8 @@ import sys
 import os
 import glob
 import re
+import json
+import requests
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Flatten, Activation
@@ -19,10 +21,12 @@ from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 import pickle
 import face_recognition as fr
+from flask_restful import Api, Resource, reqparse, marshal_with, fields
 
 # Define a flask app
 app = Flask(__name__)
-
+app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
+api = Api(app)
 # Model saved with Keras model.save()
 MODEL_PATH = 'models/face_rank_model.h5'
 
@@ -64,13 +68,40 @@ def index():
     # Main page
     return render_template('index.html')
 
+class Rankapi(Resource):
+    def post(self, message=None):
+        if request.method == 'POST':
+            res = request.get_data()
+            print(res)
+            res = json.loads(res)
+            if 'face_url' in res:
+                ret = download_img(res['face_url'])
+                if not ret:
+                    return_data = {"code":400,"msg":"图片下载失败"}
+                else:
+                    basepath = os.path.dirname(__file__)
+                    img_name = res['face_url'].split('/').pop()
+                    file_path=  os.path.join(basepath, 'uploads', img_name)
+                    preds = model_predict(file_path, model)
+                    t=round(preds[0][0]*2,3)
+                    print(preds[0])
+                    print('t',t)
+                    return_data = {"code":200,"data":{"score":str(t)},"msg":"预测成功"}
+                    return return_data
+            else:
+                return_data = {"code":401,"msg":"图片参数缺失"}
+        else:
+            return_data = {"code":402,"msg":"请求方法有误，请使用POST方法"}
+        return return_data, 200
+
+api.add_resource(Rankapi, '/rankapi')
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
+        
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
@@ -87,7 +118,7 @@ def upload():
         # result = str(pred_class[0][0][1])               # Convert to string
         t=round(preds[0][0]*2,3)
         print('t',t)
-        print(type(t))
+        # print(type(t))
         # result=t.tolist()
         # print(result)
         # print(type(result))
@@ -97,11 +128,26 @@ def upload():
         return result1
     return None
 
+def download_img(img_url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+    }
+    r = requests.get(img_url, headers=headers, stream=True)
+
+    if r.status_code == 200:
+        # 截取图片文件名
+        basepath = os.path.dirname(__file__)
+        img_name = img_url.split('/').pop()
+        file_path = os.path.join(
+            basepath, 'uploads', img_name)
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+        return True
 
 if __name__ == '__main__':
-    # app.run(port=5002, debug=True)
+    app.run(port=5002, debug=True)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    # http_server = WSGIServer(('0.0.0.0', 5000), app)
+    # http_server.serve_forever()
 
